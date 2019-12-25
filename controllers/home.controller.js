7cb3fae89a1c16cf41d3dce66a1cc438eb8ce4e4
PTChinh <PTChinh@users.noreply.github.com>
2019-12-25 11:41:34 +0700

    Handle transport and about page

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -23,5 +23,56 @@ module.exports.home = (req, res) => {
             user: req.session.user,
         });
     }
+};
+
+module.exports.transport = (req, res) => {
+
+    let us =  req.session.user;
+    let image;
+    if(us) {
+        user.findOne({
+            where: {
+                id: us.id
+            }
+        }).then(user => {
+            image = user.image;
+            res.render('home/transport', {
+                cart: req.session.cart,
+                user: req.session.user,
+                image: image
+            });
+        });
+    } else {
+        res.render('home/transport', {
+            cart: req.session.cart,
+            user: req.session.user,
+        });
+    }
+
+};
+
+module.exports.about = (req, res) => {
+
+    let us =  req.session.user;
+    let image;
+    if(us) {
+        user.findOne({
+            where: {
+                id: us.id
+            }
+        }).then(user => {
+            image = user.image;
+            res.render('home/about', {
+                cart: req.session.cart,
+                user: req.session.user,
+                image: image
+            });
+        });
+    } else {
+        res.render('home/about', {
+            cart: req.session.cart,
+            user: req.session.user,
+        });
+    }
 
 };
--- routes/home.route.js
+++ routes/home.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/home.controller');
 
 router.get('/', controller.home);
+router.get('/transport', controller.transport);
+router.get('/about', controller.about);
 
 module.exports = router;
